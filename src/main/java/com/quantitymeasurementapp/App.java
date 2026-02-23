25664cef8ec4a4825303108dfddf245648779bb8
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-02-23 11:54:38 +0530

    [Aryaman]: updated App.java with Inch Equality

--- src/main/java/com/quantitymeasurementapp/App.java
+++ src/main/java/com/quantitymeasurementapp/App.java
@@ -3,17 +3,28 @@ package com.quantitymeasurementapp;
 import java.util.Scanner;
 
 public class App {
+	
     public static void main(String[] args) {
-
-    	Scanner sc  = new Scanner(System.in);
     	
+    	Scanner sc = new Scanner(System.in);
     	int input1 = sc.nextInt();
     	int input2 = sc.nextInt();
     	
-    	Feet feet1 = new Feet(input1);
-    	Feet feet2 = new Feet(input2);
-    	
-    	System.out.println("Equal (" + feet1.equals(feet2) + ")");
-    	sc.close();
+    	demostrateFeetEquality(input1, input2);
+    	demostrateInchesEquality(input1, input2);
+    }
+    
+    public static void demostrateFeetEquality(int input1, int input2) {
+    	Feet f1 = new Feet(input1);
+        Feet f2 = new Feet(input2);
+
+        System.out.println("Are equal? " + f1.equals(f2));
+    }
+    
+    public static void demostrateInchesEquality(int input1, int input2) {
+    	Inch i1 = new Inch(input1);
+        Inch i2 = new Inch(input2);
+
+        System.out.println("Are equal? " + i1.equals(i2));
     }
 }
