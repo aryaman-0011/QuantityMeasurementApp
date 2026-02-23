b47eb6e824a67d45d487684a6feb59c353e6f26b
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-02-23 11:28:23 +0530

    [Aryaman]: updated App.java with Feet Equality

--- src/main/java/com/quantitymeasurementapp/App.java
+++ src/main/java/com/quantitymeasurementapp/App.java
@@ -1,10 +1,19 @@
 package com.quantitymeasurementapp;
 
-/**
- * Hello world!
- */
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    	Scanner sc  = new Scanner(System.in);
+    	
+    	int input1 = sc.nextInt();
+    	int input2 = sc.nextInt();
+    	
+    	Feet feet1 = new Feet(input1);
+    	Feet feet2 = new Feet(input2);
+    	
+    	System.out.println("Equal (" + feet1.equals(feet2) + ")");
+    	sc.close();
     }
 }
