f5516f54892320acc105c1419f491fbdc740442b
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-02-24 10:17:38 +0530

    [Aryaman]: Implemented Temperature measurements

--- src/main/java/com/quantitymeasurementapp/IMeasurable.java
+++ src/main/java/com/quantitymeasurementapp/IMeasurable.java
@@ -2,7 +2,22 @@ package com.quantitymeasurementapp;
 
 public interface IMeasurable {
 
-    double convertToBaseUnit(double value);
+	double convertToBaseUnit(double value);
 
-    double convertFromBaseUnit(double baseValue);
+	double convertFromBaseUnit(double baseValue);
+
+	@FunctionalInterface
+	interface SupportsArithmetic {
+		boolean isSupported();
+	}
+
+	SupportsArithmetic supportsArithmetic = () -> true;
+
+	default boolean supportsArithmetic() {
+		return supportsArithmetic.isSupported();
+	}
+
+	default void validateOperationSupport(String operation) {
+		// Overridden in TemperatureUnit
+	}
 }
