603db2ae940b6efabd2070c854be356a79a4757b
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2026-02-24 09:57:42 +0530

    [Aryaman]: Implemented Centralized Arithmetic operations

--- src/main/java/com/quantitymeasurementapp/IMeasurable.java
+++ src/main/java/com/quantitymeasurementapp/IMeasurable.java
@@ -1,15 +1,8 @@
 package com.quantitymeasurementapp;
 
 public interface IMeasurable {
-	double getConversionFactor();
 
-	double convertToBaseUnit(double value);
+    double convertToBaseUnit(double value);
 
-	double convertFromBaseUnit(double baseValue);
-
-	String getUnitName();
-
-	public static void main(String[] args) {
-		System.out.println("IMeasurable Interface");
-	}
+    double convertFromBaseUnit(double baseValue);
 }
